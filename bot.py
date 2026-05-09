323fa41e7105423494fc11c1ded54c251bac89a2
rehanytdatatg-ops <rehanytdatatg@gmail.com>
2026-05-09 19:47:09 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,46 +1,34 @@
-# Simple Telegram Premium Access Bot
-# Install:
-# pip install pyTelegramBotAPI
-
 import telebot
 
 TOKEN = "8599821431:AAHWT4Qr57pRZk_5w3lZrkm3Tyvp2Ga8uMo"
-ADMIN = "@Yhunalm"
 
 bot = telebot.TeleBot(TOKEN)
 
-WELCOME = f"""
+@bot.message_handler(commands=['start'])
+def start(message):
+
+    photo = open("qr.jpg", "rb")
+
+    bot.send_photo(
+        message.chat.id,
+        photo,
+        caption="""
 🔥 PREMIUM VIDEO ACCESS 🔥
 
 💎 Price: ₹99 Only
 
-✅ Pay using QR Code
-📸 Send payment screenshot after payment
+✅ Scan QR & Pay
+📸 Send payment screenshot
 
-⏳ Access will be given after verification.
-
-📩 Admin: {ADMIN}
+📩 Admin: @Yhunalm
 """
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, WELCOME)
-
-@bot.message_handler(content_types=['photo'])
-def payment_screenshot(message):
-    bot.reply_to(
-        message,
-        "✅ Screenshot received.\nAdmin will verify payment soon."
     )
 
-    # Forward screenshot to admin
-    # Replace ADMIN_CHAT_ID with your Telegram numeric ID
-    ADMIN_CHAT_ID = 
-7831401352
-    bot.forward_message(
-        ADMIN_CHAT_ID,
-        message.chat.id,
-        message.message_id
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    bot.reply_to(
+        message,
+        "✅ Screenshot received.\nAdmin will verify payment."
     )
 
 print("Bot Running...")
